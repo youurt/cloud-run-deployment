b8713d567f8811619376cad481351bd104a09c36
Ugur Tigu <ugurtigu@gmail.com>
2020-10-28 16:15:46 +0100

    testing deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log("New Connection")
-    res.end("TEST SERVER")
+    res.end("TEST SERVER --- continuous deployment")
 })
 
 const PORT = process.env.PORT || 8080
