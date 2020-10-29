6937b7ef8c81807d920b35c08d2a6fbdc89742e8
Ugur Tigu <ugurtigu@gmail.com>
2020-10-29 15:10:39 +0100

    show time as string

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log("New Connection")
-    res.end('TEST SERVER --- continuous deployment')
+    const nowTime = Date.now()
+    res.end(`TEST SERVER --- continuous deployment ${nowTime}`)
 })
 
 const PORT = process.env.PORT || 8080
